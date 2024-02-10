ff2bd072609a11d86ad93d996f3afd5268c1f300
Alan Pogrebinschi <alanpog@gmail.com>
2024-02-10 11:08:17 -0500

    runs buildMap in bext dev

--- src/bext.ts
+++ src/bext.ts
@@ -3,10 +3,39 @@ import { spawn } from "child_process";
 import path from "path";
 
 const args = process.argv.slice(2);
-
 const helpMessage = "I'm the bext script.";
 
-const runDev = () => {
+const runDev = async () => {
+  // Determine the path to the buildMap binary
+  const buildMapPath = path.join(
+    process.cwd(),
+    "node_modules",
+    ".bin",
+    "buildMap"
+  );
+
+  // Execute buildMap before starting the dev server
+  console.log("Running buildMap...");
+  const buildMapProcess = spawn(buildMapPath, [], { stdio: "inherit" });
+
+  await new Promise((resolve, reject) => {
+    buildMapProcess.on("error", (err) => {
+      console.error(`Failed to start buildMap process: ${err.message}`);
+      reject(err);
+    });
+
+    buildMapProcess.on("exit", (code) => {
+      if (code === 0) {
+        console.log("buildMap completed successfully.");
+        resolve(code);
+      } else {
+        console.error(`buildMap exited with code ${code}`);
+        reject(new Error("buildMap failed"));
+      }
+    });
+  });
+
+  // After buildMap has successfully completed, start the Bun process
   const scriptPath = path.join(process.cwd(), "src", "index.tsx");
   const bunProcess = spawn("bun", ["run", "--watch", scriptPath], {
     stdio: "inherit",
